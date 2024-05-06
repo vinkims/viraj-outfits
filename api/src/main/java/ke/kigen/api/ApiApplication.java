8ea6fb7f2d1236725f8050d4c486da477950dc73
vinkims <vincekigen@gmail.com>
2024-05-06 14:58:07 +0300

    feat[Cors]: Fix cors issue

--- api/src/main/java/ke/kigen/api/ApiApplication.java
+++ api/src/main/java/ke/kigen/api/ApiApplication.java
@@ -2,12 +2,28 @@ package ke.kigen.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOriginPatterns("*")
+					.allowedMethods("GET", "POST", "PATCH", "HEAD", "OPTIONS")
+					.allowCredentials(true);
+			}
+		};
 	}
 
 }
