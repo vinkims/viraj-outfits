3bfa9c0db3cf28f952515565899e7552eefd9bd9
vinkims <vincekigen@gmail.com>
2024-06-06 10:49:33 +0300

    feat[Items]: Add data to table

--- ui/src/screens/ItemsScreen.jsx
+++ ui/src/screens/ItemsScreen.jsx
@@ -1,5 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Alert, Card, Container, Grid, Snackbar, TableContainer } from "@mui/material";
+import { 
+  Alert, 
+  Card, 
+  Container, 
+  Grid, 
+  Snackbar, 
+  Table, 
+  TableBody, 
+  TableCell, 
+  TableContainer, 
+  TableHead, 
+  TableRow 
+} from "@mui/material";
 
 import ServerCommunicationUtils from "../utils/ServerCommunicationUtils";
 
@@ -20,6 +32,26 @@ const ItemsScreen = () => {
     await ServerCommunicationUtils.get('item')
     .then(data => {
       console.log("Data: ", data);
+      if (data.status === 200) {
+        setLoading(false);
+        setItems(data.content.data);
+      } else {
+        setLoading(false);
+        setOpenAlert(true);
+        setSeverity('error');
+        setAlertMessage('Error fetching items');
+      }
+    })
+    .catch(error => {
+      console.error("Error: ", error);
+      setLoading(false);
+      setOpenAlert(true);
+      setSeverity('error');
+      if (error.toString().includes('NetworkError when attempting to fetch resource')) {
+        setAlertMessage('Please check your internet connection');
+      } else {
+        setAlertMessage('Error fetching items');
+      }
     })
   }
 
@@ -47,10 +79,37 @@ const ItemsScreen = () => {
       <Grid container spacing={2}>
         <Card>
           <TableContainer>
-
+            <Table sx={{ minWidth: 650 }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Item Type</TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Category</TableCell>
+                  <TableCell>Color</TableCell>
+                  <TableCell>Size</TableCell>
+                  <TableCell>Price</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {items.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell>{row.itemType.name}</TableCell>
+                    <TableCell>{row.name}</TableCell>
+                    <TableCell>{row.category.name}</TableCell>
+                    <TableCell>{row.color}</TableCell>
+                    <TableCell>{row.size}</TableCell>
+                    <TableCell>{row.price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
           </TableContainer>
         </Card>
       </Grid>
+      {showAlert()}
     </Container>
   );
 }
